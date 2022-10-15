558b9101a2b5b9e4e2686157b1ded40659db8268
Alexander Angelov <alexander.angelov@mentormate.com>
2022-10-15 03:07:56 +0300

    Initial Commit

---
+++ Set/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Set
+//
+//  Created by Alexander Angelov on 15.10.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
