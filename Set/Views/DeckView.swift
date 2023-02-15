c845b550a7eea9bc4b7dfa5718d97d96ed842915
Alexander Angelov <alexander.angelov@mentormate.com>
2023-02-15 22:52:46 +0200

    Start implementing animation to deal cards from the deck 1. Add a DeckView class with enum depending if its empty or full deck

---
+++ Set/Views/DeckView.swift
@@ -0,0 +1,50 @@
+//
+//  DeckView.swift
+//  Set
+//
+//  Created by Alexander Angelov on 15.02.23.
+//
+
+import UIKit
+
+class DeckView: UIImageView {
+    
+    var label: UILabel?
+    
+    enum State {
+        case notEmpty
+        case empty
+        
+        var text: String {
+            switch self {
+                case .notEmpty:
+                    return "Deal 3"
+                case .empty:
+                    return ""
+            }
+        }
+        
+        var image: UIImage? {
+            switch self {
+                case .notEmpty:
+                    return UIImage(named: "cardBack")
+                case .empty:
+                    return nil
+            }
+        }
+    }
+    
+    func configureDeck() {
+        layer.cornerRadius = CardViewConstant.cornerRadius
+        translatesAutoresizingMaskIntoConstraints = false
+        isUserInteractionEnabled = true
+    }
+    
+    func configureDiscardPile() {
+        label = UILabel()
+        label?.textColor = .red
+        label?.textAlignment = .center
+        label?.font = .systemFont(ofSize: 25, weight: .bold)
+        addSubview(label!)
+    }
+}
