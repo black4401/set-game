28a77d5f2ae88ac7e6832b0e8febd52d31d05a91
Alexander Angelov <alexander.angelov@mentormate.com>
2023-02-17 13:31:44 +0200

    Add extension to UIView with shake animation method

---
+++ Set/Exensions/UIView+ShakeAnimation.swift
@@ -0,0 +1,21 @@
+//
+//  UIView+ShakeAnimation.swift
+//  Set
+//
+//  Created by Alexander Angelov on 17.02.23.
+//
+
+import UIKit
+
+extension UIView{
+    
+    func shake(){
+        let animation = CABasicAnimation()
+        animation.duration = 0.04
+        animation.repeatCount = 5
+        animation.autoreverses = true
+        animation.fromValue = NSValue(cgPoint: CGPointMake(self.center.x - 8.0, self.center.y))
+        animation.toValue = NSValue(cgPoint: CGPointMake(self.center.x + 10.0, self.center.y))
+        self.layer.add(animation, forKey: "position")
+    }
+}
